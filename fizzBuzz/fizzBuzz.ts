96f2206acf773085b678cb202f452f72a133e730
ilya.medukha <i.medukha@gmail.com>
2021-09-16 18:17:35 +0300

    fixed after review.

--- fizzBuzz/fizzBuzz.ts
+++ fizzBuzz/fizzBuzz.ts
@@ -2,7 +2,7 @@
 const fizzBuzz = (num: number) => {
   for (let i = 1; i <= num; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
-      console.log('fizzBuzz');
+      console.log('FizzBuzz');
     } else if (i % 3 === 0) {
       console.log('fizz');
     } else if (i % 5 === 0) {
