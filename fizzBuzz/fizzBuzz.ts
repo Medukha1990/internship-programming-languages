c749b05161bdf1a96b594e6cf8ec8a7d30239efe
ilya.medukha <i.medukha@gmail.com>
2021-09-16 18:10:30 +0300

    fixed after review

--- fizzBuzz/fizzBuzz.ts
+++ fizzBuzz/fizzBuzz.ts
@@ -1,8 +1,16 @@
+
 const fizzBuzz = (num: number) => {
-  for (let i = 1; i < num; i++) {
-    const str = (i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz');
-    console.log(str === '' ? i : str);
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
   }
-};
+}
 
 export default fizzBuzz;
